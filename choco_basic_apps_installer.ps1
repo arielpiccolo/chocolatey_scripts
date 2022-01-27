3214f8b2df92447cd608e03711a360714a5ae9fd
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2022-01-27 11:41:27 +0000

    Update choco_basic_apps_installer.ps1

--- choco_basic_apps_installer.ps1
+++ choco_basic_apps_installer.ps1
@@ -1,1 +1,36 @@
-﻿.
+$basicAppList = "googlechrome,firefox,microsoft-edge,vscode,postman,filezilla,slack,zoom,notepadplusplus"
+#$devsAppList = ""
+
+
+
+echo "Looking for Chocolatey in the system..."
+echo ""
+try{
+    choco config get cacheLocation
+}catch{
+     Write-Output "Chocolatey not detected, trying to install now"
+     echo " "
+    #iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    iex ((Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))))
+}
+
+echo " "
+
+    if ([string]::IsNullOrWhiteSpace($basicyAppList) -eq $false){   
+    Write-Host "Chocolatey Apps Specified"  
+    echo " "    
+
+    $appsToInstall = $basicAppList -split "," | foreach { "$($_.Trim())" }
+    #$appsToInstall = $devsAppList -split "," | foreach { "$($_.Trim())" }
+
+    foreach ($app in $appsToInstall)
+    {
+        Write-Host "Installing $app"
+        & choco install $app /y
+    }
+
+}
+
+
+
+
