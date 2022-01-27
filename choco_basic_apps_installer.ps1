3e91612feba6308de7c5526f15fe1379e99f4173
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2022-01-27 12:56:26 +0000

    Update choco_basic_apps_installer.ps1

--- choco_basic_apps_installer.ps1
+++ choco_basic_apps_installer.ps1
@@ -1,35 +1,25 @@
 $basicAppList = "googlechrome,firefox,microsoft-edge,vscode,postman,filezilla,slack,zoom,notepadplusplus"
 #$devsAppList = ""
 
-
-
 echo "Looking for Chocolatey in the system..."
 echo ""
+
 try{
     choco config get cacheLocation
 }catch{
      Write-Output "Chocolatey not detected, trying to install now"
      echo " "
-     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
+     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  
+     echo " "    
 
-echo " "
+     $appsToInstall = $basicAppList -split "," | foreach { "$($_.Trim())" }
+     #$appsToInstall = $devsAppList -split "," | foreach { "$($_.Trim())" }
 
-    if ([string]::IsNullOrWhiteSpace($basicyAppList) -eq $false){   
-    Write-Host "Chocolatey Apps Specified"  
-    echo " "    
-
-    $appsToInstall = $basicAppList -split "," | foreach { "$($_.Trim())" }
-    #$appsToInstall = $devsAppList -split "," | foreach { "$($_.Trim())" }
-
-    foreach ($app in $appsToInstall)
-    {
-        Write-Host "Installing $app"
-        & choco install $app /y
-    }
+     foreach ($app in $appsToInstall)
+     {
+         Write-Host "Installing $app"
+         & choco install $app /y
+     }
 
 }
-
-
-
-
