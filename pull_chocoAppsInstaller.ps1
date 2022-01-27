694008be2018e726ae8f281d804862a325c8378c
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2022-01-27 10:55:28 +0000

    adding auto downloader for backup

---
+++ pull_chocoAppsInstaller.ps1
@@ -0,0 +1,16 @@
+﻿clear
+echo "Downloading chocoAppsInstaller"
+echo ""
+Invoke-WebRequest https://github.com/arielpiccolo/chocolatey_scripts/archive/refs/heads/main.zip  -OutFile ./main.zip
+Start-Sleep -s 2
+clear
+$filepath = Resolve-Path "main.zip"
+Expand-Archive "$filepath" -Force
+Remove-Item *.zip
+Start-Sleep -s 2
+clear
+echo ""
+echo "Extracting Files..."
+echo ""
+Start-Sleep -s 2
+echo "Done!"
